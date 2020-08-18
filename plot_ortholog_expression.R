09b1bce1d7db38f56148baa13155da538a09e4c3
Jason Stajich <jason.stajich@ucr.edu>
2020-08-18 16:46:48 +0000

    add in a filtering step example

--- plot_ortholog_expression.R
+++ plot_ortholog_expression.R
@@ -4,8 +4,12 @@ library(tidyverse)
 
 orthtable <- read_delim("Citrus_Orthologs_v20200817.tsv",delim="\t",
                         col_names = c("OrthoGroup","Cs", "Cr", "Cs.expr","Cr.expr"))
+length(orthtable$OrthoGroup) # how many rows?
+orthtable.filter = orthtable %>% filter(Cs.expr > 0 & Cr.expr > 0)
+length(orthtable.filter$OrthoGroup) # how many rows?
+
 pdf("ggplots.pdf")
-ggplot(orthtable, aes(x=log(Cs.expr), y=log(Cr.expr))) + geom_point()
-ggplot(orthtable, aes(x=Cs.expr, y=Cr.expr)) + geom_point()
-cor(orthtable$Cs.expr,orthtable$Cr.expr)
+ggplot(orthtable.filter, aes(x=log(Cs.expr), y=log(Cr.expr))) + geom_point()
+ggplot(orthtable.filter, aes(x=Cs.expr, y=Cr.expr)) + geom_point()
+cor(orthtable.filter$Cs.expr,orthtable.filter$Cr.expr)
 dev.off()
