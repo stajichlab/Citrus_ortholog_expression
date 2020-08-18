e493b2066222eb0d7c06b5340d9d88bd2a08b2b6
Jason Stajich <jason.stajich@ucr.edu>
2020-08-18 04:47:42 +0000

    Basic plotting

--- plot_ortholog_expression.R
+++ plot_ortholog_expression.R
@@ -2,3 +2,9 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 
+orthtable <- read_delim("Citrus_Orthologs_v20200817.tsv",delim="\t",
+                        col_names = c("OrthoGroup","Cs", "Cr", "Cs.expr","Cr.expr"))
+ggplot(orthtable, aes(x=log(Cs.expr), y=log(Cr.expr))) + geom_point()
+ggplot(orthtable, aes(x=Cs.expr, y=Cr.expr)) + geom_point()
+cor(orthtable$Cs.expr,orthtable$Cr.expr)
+
