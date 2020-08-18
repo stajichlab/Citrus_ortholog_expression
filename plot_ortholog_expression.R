9226ac39960c9132087dfed278d5b50bfb1f8184
Jason Stajich <jason.stajich@ucr.edu>
2020-08-18 04:48:51 +0000

    save plots to file

--- plot_ortholog_expression.R
+++ plot_ortholog_expression.R
@@ -4,7 +4,8 @@ library(tidyverse)
 
 orthtable <- read_delim("Citrus_Orthologs_v20200817.tsv",delim="\t",
                         col_names = c("OrthoGroup","Cs", "Cr", "Cs.expr","Cr.expr"))
+pdf("ggplots.pdf")
 ggplot(orthtable, aes(x=log(Cs.expr), y=log(Cr.expr))) + geom_point()
 ggplot(orthtable, aes(x=Cs.expr, y=Cr.expr)) + geom_point()
 cor(orthtable$Cs.expr,orthtable$Cr.expr)
-
+dev.off()
