b9741c9882285e6f96cfbe3fa49c90b7a3514b1b
Jason Stajich <jason.stajich@ucr.edu>
2020-08-20 23:07:29 +0000

    show how to filter

--- plot_ortholog_expression.R
+++ plot_ortholog_expression.R
@@ -5,11 +5,21 @@ library(tidyverse)
 orthtable <- read_delim("Citrus_Orthologs_v20200817.tsv",delim="\t",
                         col_names = c("OrthoGroup","Cs", "Cr", "Cs.expr","Cr.expr"))
 length(orthtable$OrthoGroup) # how many rows?
-orthtable.filter = orthtable %>% filter(Cs.expr > 0 & Cr.expr > 0)
+orthtable.filter = orthtable %>% filter(Cs.expr > 10 & Cr.expr > 10)
 length(orthtable.filter$OrthoGroup) # how many rows?
+
+orthtable.filter$foldchange <- log(orthtable.filter$Cs.expr/orthtable.filter$Cr.expr)/log(2)
+hist(orthtable.filter$foldchange,breaks=100)
+
+twofold_upDown = orthtable.filter %>% filter(foldchange > 1 | foldchange < -1)
+#  this is the same thing as above
+#twofold_upDown = subset(orthtable.filter, orthtable.filter$foldchange > 1 | orthtable.filter$foldchange < -1)
+hist(twofold_upDown$foldchange,breaks=100)
+ggplot(twofold_upDown, aes(x=log(Cs.expr), y=log(Cr.expr))) + geom_point()
 
 pdf("ggplots.pdf")
 ggplot(orthtable.filter, aes(x=log(Cs.expr), y=log(Cr.expr))) + geom_point()
 ggplot(orthtable.filter, aes(x=Cs.expr, y=Cr.expr)) + geom_point()
 cor(orthtable.filter$Cs.expr,orthtable.filter$Cr.expr)
+
 dev.off()
