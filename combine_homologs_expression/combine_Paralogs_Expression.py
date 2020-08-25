098a9c678f1b90fcd4ea13b041371af8bb5654b5
Jason Stajich <jasonstajich.phd@gmail.com>
2020-08-25 00:44:53 -0700

    add code for paralog report

---
+++ combine_homologs_expression/combine_Paralogs_Expression.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Author: Talieh Ostovar
+
+import re, sys, csv
+
+
+singlecopy="Orthogroups_SingleCopyOrthologues.txt"
+orthogroups="Orthogroups.txt"
+
+TPMfiles = ['TPM_reticulata_embryo_r1.tsv','TPM_sinensis_embryo_r1.tsv']
+
+singlecopyOG = set() #  a set is like a list but it is only a unique set of items
+
+# read in the names of the single copy orthologs to keep
+with open(singlecopy,"r") as sc:
+    for line in sc:
+        singlecopyOG.add(line.strip())
+
+# read in the orthogroups
+OGdata = {}
+
+TPMs = {} # can assume that gene names are unique so put them all in one place
+# this structure needs to change when you want to incorporate multiple
+# expression timepoints/replicates
+# loop through the names of the files
+for TPMfile in TPMfiles:
+    # open the file
+    with open(TPMfile,"r") as tsvfile:
+        # if they really were tab delimited use this
+        #exprdr = csv.reader(tsvfile, delimiter='\t')
+        # parse file with csv module in python
+        exprdr = csv.reader(tsvfile, delimiter=' ')
+        header=next(exprdr)
+        for row in exprdr:
+            (gene_name,TPM) = row
+            TPMs[gene_name] = TPM
+
+
+print("\t".join(["Orthogroup",'Species','Gene','TPM']))
+
+with open(orthogroups,"r") as og:
+    for line in og:
+        # split the Orthogroups line by whitespace (space)
+        gene_names = line.split()
+        # take first item off this is Orthogroup ID
+        # remove the ':' in the name at same time
+        orthogroupID = re.sub(":","",gene_names.pop(0))
+        if orthogroupID not in singlecopyOG:
+            og = dict()
+            for gene in gene_names:
+                if gene.startswith("MSY"):
+                    print("\t".join([orthogroupID,'Cr',gene,TPMs[gene]]))
+                elif gene.startswith("Cs") or gene.startswith("orange"):
+                    print("\t".join([orthogroupID,'Cs',gene,TPMs[gene]]))
+                else:
+                    sys.stderr.write("Unknown gene pattern for '%s' cannot guess Species"%(gene))
