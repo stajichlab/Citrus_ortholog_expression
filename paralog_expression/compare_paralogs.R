b126e9990385180b9cd9c28511d30cf646943784
Jason Stajich <jasonstajich.phd@gmail.com>
2020-08-26 21:29:28 -0700

    Paralog viewing plot

---
+++ paralog_expression/compare_paralogs.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+TPM <- read_delim("Citrus_paralogs_TPM.tsv",delim="\t",col_names=TRUE)
+
+SumExpr <- TPM %>% filter( Expr > 0) %>% group_by(Orthogroup,Species) %>%
+  summarise(avg = mean(Expr),
+           # median = median(Expr),
+            #max = max(Expr), 
+            #min = min(Expr), 
+            #n = n(),
+            ) %>% 
+  arrange(avg)
+
+SumExpr.Cs <- SumExpr %>% filter(Species == "Cs") %>% rename(Cs.avg = avg) %>% select( -Species)
+SumExpr.Cr <- SumExpr %>% filter(Species == "Cr") %>% rename(Cr.avg = avg) %>% select( -Species)
+Combine = inner_join(SumExpr.Cs, SumExpr.Cr)
+plot(Combine$Cs.avg,Combine$Cr.avg)
