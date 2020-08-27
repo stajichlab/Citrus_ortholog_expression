76014698a2d5bb96595f732e4df08b21ae5793a8
Jason Stajich <jasonstajich.phd@gmail.com>
2020-08-26 21:29:51 -0700

    add log of plots

--- paralog_expression/compare_paralogs.R
+++ paralog_expression/compare_paralogs.R
@@ -16,4 +16,5 @@ SumExpr <- TPM %>% filter( Expr > 0) %>% group_by(Orthogroup,Species) %>%
 SumExpr.Cs <- SumExpr %>% filter(Species == "Cs") %>% rename(Cs.avg = avg) %>% select( -Species)
 SumExpr.Cr <- SumExpr %>% filter(Species == "Cr") %>% rename(Cr.avg = avg) %>% select( -Species)
 Combine = inner_join(SumExpr.Cs, SumExpr.Cr)
+plot(log(Combine$Cs.avg),log(Combine$Cr.avg))
 plot(Combine$Cs.avg,Combine$Cr.avg)
