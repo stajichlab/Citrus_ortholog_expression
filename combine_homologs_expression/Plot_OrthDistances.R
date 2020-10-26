e80c8ab94b27e982c3ba4db1dd1371880204f373
Jason Stajich <jasonstajich.phd@gmail.com>
2020-10-25 20:41:51 -0700

    some more ploting

--- combine_homologs_expression/Plot_OrthDistances.R
+++ combine_homologs_expression/Plot_OrthDistances.R
@@ -15,7 +15,7 @@ orthdistFilt <- orthdist %>%
     mutate( Cr.bin = if_else( Crdist == 0 | Crdist > 2000, 0, round(Crdist / 100))) %>%
     mutate( Cs.bin = if_else( Csdist == 0 | Csdist > 2000, 0, round(Csdist / 100))) %>%
     mutate( Cs.bin = if_else( Cs.bin < 0, -1, Cs.bin) ) %>% 
-    mutate( Cr.bin = if_else( Cr.bin < 0, -1, Cr.bin) ) 
+    mutate( Cr.bin = if_else( Cr.bin < 0, -1, Cr.bin) ) %>% filter(Cs.expr > 0 | Cr.expr > 0)
 
 p <- ggplot(orthdistFilt, aes(Cr.bin,Cr.expr)) +
   geom_bar(position = 'dodge', stat = 'summary') +
@@ -33,6 +33,12 @@ ggplot(orthdistFilt) + geom_histogram(aes(x = Cr.expr,fill=Cr.bin),binwidth=100)
 ggplot(orthdistFilt) + geom_histogram(aes(x = Cs.expr,fill=Cs.bin),binwidth=100) +
   facet_wrap( Cs.bin ~ ., scales="free") 
 
+CrTE <- orthdistFilt %>% filter(Cs.bin == 0) %>% filter(Cr.bin > 0)
+CsTE <- orthdistFilt %>% filter(Cr.bin == 0) %>% filter(Cs.bin > 0)
+
+ggplot(CrTE) + geom_point(aes(x=Cs.expr,y=Cr.expr,fill=Cr.bin,color=Cr.bin))
+
+ggplot(CsTE) + geom_point(aes(x=Cr.expr,y=Cs.expr,fill=Cs.bin,color=Cs.bin))
 # old
 
 #Crmed <- orthdist %>% filter(Crdist > 0) %>% filter(Crdist < 2000) %>%
